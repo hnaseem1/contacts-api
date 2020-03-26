1ece02111cf3d1ac9b65984e98a494df265e2e7b
Hasan Naseem <hn92@live.com>
2020-03-26 00:28:48 -0400

    added routes

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
+// FileName: index.js
+// Import express
 let express = require('express')
-
+// Initialize the app
 let app = express();
-
+// Setup server port
 var port = process.env.PORT || 8080;
-
+// Send message for default URL
 app.get('/', (req, res) => res.send('Hello World with Express'));
-
-app.listen(port, function() {
-    console.log("Running API on port" + port);
-  }
-)
+// Launch app to listen to specified port
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
+// Import routes
+let apiRoutes = require("./api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
